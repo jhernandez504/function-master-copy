70ec995e9bd23210b82ab2d5e31f51a4e962f4be
jhernandez504 <jeremy.hernandez504@gmail.com>
2024-05-31 19:03:51 -0500

    tried fixing nonfriends()

--- function-master.js
+++ function-master.js
@@ -128,7 +128,13 @@ function profileInfo(object) {
 //////////////////////////////////////////////////////////////////////
 
 function maybeNoises(object) {
-
+    //check if object has arraynoises and length is > 0
+    if (object.hasOwnProperty('noises') && object['noises'].length > 0){
+        let arrayToSentence = object['noises'].join(' ');
+        return arrayToSentence;
+    } else if(!object.hasOwnProperty('noises') || object['noises'].length === 0){
+        return 'there are no noises';
+    }
 }
 
 //////////////////////////////////////////////////////////////////////
@@ -136,15 +142,27 @@ function maybeNoises(object) {
 //////////////////////////////////////////////////////////////////////
 
 function hasWord(string, word) {
-
+    //split string into array
+    let stringArray = string.split(' ');
+    //iterate thru array to search if index value is == to word
+    for (let i = 0; i < stringArray.length; i++){
+        if(stringArray[i] == word){
+            return true;
+        } 
+        
+    }
+  return false;
 }
+
+
 
 //////////////////////////////////////////////////////////////////////
 // Function 11 - Add Friend //////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 
 function addFriend (name, object) {
-
+    object['friends'].push(name);
+    return object;
 }
 
 //////////////////////////////////////////////////////////////////////
@@ -152,23 +170,57 @@ function addFriend (name, object) {
 //////////////////////////////////////////////////////////////////////
 
 function isFriend(name, object) {
-
+    //if object has key friends
+    if(object.hasOwnProperty('friends')){
+        //iterate thru array to search if index value is == to word
+        for (let i = 0; i < object['friends'].length; i++){
+        //if object's friend index is = to name returns true
+        if(object['friends'][i] == name){
+           return true;
+       } 
+       
+   }
 }
+ return false;
+}
+
 
 //////////////////////////////////////////////////////////////////////
 // Function 13 - Non-Friends /////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 
-function nonFriends(name, array) {
-
+function nonFriends(name, array) { 
+    //array to hold names not friends 
+    let notAFriend = [];
+    let friends = [];
+    //for of loop to iterate thru every obj in the array
+    for (let obj of array){
+        //if condition check if object's name is = to name 
+        if(obj.name === name){
+            //if true assigns object's friends array to friends empty array
+            friends = obj.friends;
+            //breaks loop once name = objects name
+            break;
+        }
+    }
+    //for of loop objects in array
+    for (let obj of array){
+        //checks if name !== to objects name while friends array doesnt include object'name
+        if (obj.name !== name && !friends.includes(obj.name)){
+            //pushes object name to notAFriend array 
+            notAFriend.push(obj.name);
+        }
+    }
+    //returns whole notAFriendArray once done
+    return notAFriend;
 }
-
 //////////////////////////////////////////////////////////////////////
 // Function 14 - Update Object ///////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 
 function updateObject(object, key, value) {
-
+    object[key] = value;
+    return object;
 }
 
 //////////////////////////////////////////////////////////////////////
@@ -176,6 +228,19 @@ function updateObject(object, key, value) {
 //////////////////////////////////////////////////////////////////////
 
 function removeProperties(object, array) {
+    let tempObject = object;
+    //iterates thru array
+    for (let i = 0; i < array.length; i++){
+      //create key from array[i]
+      let keyName = array[i];
+      //if condition to check if object has a key listed from array at index
+      if(tempObject.hasOwnProperty(keyName)){
+        //delete object key 
+        delete tempObject[keyName];
+
+      }  
+      return tempObject;
+    }
 
 }
 
